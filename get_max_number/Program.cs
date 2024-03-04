adb991f068e0c36a585d92d6d7638f04d48e610a
DaviesNyantakyi <davies.nyantakyi@outlook.col>
2024-03-05 00:42:37 +0100

    method created

---
+++ get_max_number/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+class MyApp
+{
+
+    class Human
+    {
+        static void Main()
+        {
+
+
+          int result =   GetMaxNumber(2, 5);
+
+            Console.WriteLine(result);
+        }
+
+        static int GetMaxNumber(int num1, int num2)
+        {
+           if(num1> num2)
+            {
+               return num1;
+            }else
+            {
+                return num2;
+            }
+        }
+    }
+}
